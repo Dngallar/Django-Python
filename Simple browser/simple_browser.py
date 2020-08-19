c506ae3e8832e012a9c5fc6982070b02bed3f498
Domingo Gallardo Saavedra <dngallar@uc.cl>
2020-08-18 22:54:53 -0400

    Code of the simplest broser in Python

---
+++ Simple browser/simple_browser.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+    Simplest Browser in Python
+
+    Domingo Gallardo Saavedra
+    August, 18 2020
+"""
+
+import socket
+
+# End point to send and receive data to the computer
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org' , 80)) # Call
+# Data in the Internet is generally sent in UTF-8. In Pytho is Unicode
+cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512) # Up to 512 characters
+    if len(data) < 1:
+        break
+    print(data.decode(), end = '')
+
+mysock.close()
