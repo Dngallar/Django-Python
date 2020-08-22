e2e99023304e7c88db0703176d0abfbb7aeaa995
Domingo Gallardo Saavedra <dngallar@uc.cl>
2020-08-22 19:05:49 -0400

    Update setting file

--- SiteTest_2/SiteTest_2/settings.py
+++ SiteTest_2/SiteTest_2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(a99y!qloai^d0jyq0)k1fi21dqnpn=9km5^$!ocykumr27ykj'
+SECRET_KEY = 'ADD -> CORRECT KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
