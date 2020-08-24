df8c1b6343779ae9320391ce05332f7576267180
Domingo Gallardo Saavedra <dngallar@uc.cl>
2020-08-24 18:53:39 -0400

    SiteTest_4 code update

--- SiteTest_4/SiteTest_4/urls.py
+++ SiteTest_4/SiteTest_4/urls.py
@@ -1,4 +1,4 @@
-"""SiteTest_3 URL Configuration
+"""SiteTest_4 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
