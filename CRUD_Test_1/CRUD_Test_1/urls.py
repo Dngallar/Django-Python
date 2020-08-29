5baa31ac4ad842ebd46bf7f89bbe3282d890f150
Domingo Gallardo Saavedra <dngallar@uc.cl>
2020-08-29 18:53:43 -0400

    CRUD_Test_1 update

--- CRUD_Test_1/CRUD_Test_1/urls.py
+++ CRUD_Test_1/CRUD_Test_1/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('autos/', include('autos.urls')),
+    path('home/', include('home.urls')),
 ]
